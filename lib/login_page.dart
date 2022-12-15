db65c085d7159761af06d224b70c2fe8fc5a62aa
Thiennguyen21it <thiennh.21it@vku.udn.vn>
2022-12-15 19:34:22 +0700

    login with phone firebase

---
+++ lib/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo Login wih Phone'),
+      ),
+      body: const Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
